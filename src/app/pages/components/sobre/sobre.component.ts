afc432b9314309f60877ad8036cf5dbb12233e5f
Eliel Lopes Dos Santos <ellsantos@rd.com.br>
2023-03-15 11:54:46 -0300

    reactive forms tipados

--- src/app/pages/components/sobre/sobre.component.ts
+++ src/app/pages/components/sobre/sobre.component.ts
@@ -1,18 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-sobre',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './sobre.component.html',
-  styleUrls: ['./sobre.component.scss']
+  styleUrls: ['./sobre.component.scss'],
 })
 export class SobreComponent implements OnInit {
+  public form = this.formBuilder.group({
+    email: ['eliel@email.com'],
+    password: [''],
+  });
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
+    console.log(this.form.value.email);
   }
-
 }
